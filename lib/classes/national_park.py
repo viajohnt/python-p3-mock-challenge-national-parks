06441fd0ea710350ade9676cf871ce0c60302829
John Truong <viajohnt@gmail.com>
2023-05-09 16:01:44 -0600

    Final Commit

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -4,17 +4,44 @@ class NationalPark:
         self.name = name
         self._trips = []
         self._visitors = []
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if isinstance (new_name, str) and not hasattr (self, 'name'):
+            self._name = new_name
+        else:
+            raise Exception
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return self._trips
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
-        pass
+        if isinstance(new_visitor, Visitor) and new_visitor not in self._visitors:
+            self._visitors.append(new_visitor)
+        return self._visitors
     
     def total_visits(self):
-        pass
+        return len(self._trips)
     
     def best_visitor(self):
-        pass
+        if self._visitors:
+            return max(self._visitors, key=lambda visitor: len(visitor._trips))
+        else:
+            return None
+        
+        # max_visitor = None
+        # max_visits = 0
+        # for v in self._visitors:
+        #     v_visits = len([t for t in self._trips if t.visitor == v])
+        #     if v_visits > max_visits:
+        #         max_visits = v_visits
+        #         max_visitor = v
+        # return max_visitor
--- lib/classes/trip.py
+++ lib/classes/trip.py
@@ -6,3 +6,32 @@ class Trip:
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+        visitor.trips(self)
+        visitor.national_parks(national_park)
+        national_park.trips(self)
+        national_park.visitors(visitor)
+        
+
+    @property
+    def visitor(self):
+        return self._visitor
+    
+    @visitor.setter
+    def visitor(self, new_visitor):
+        from classes.visitor import Visitor
+        if isinstance(new_visitor, Visitor):
+            self._visitor = new_visitor
+        else:
+            raise Exception
+        
+    @property
+    def national_park(self):
+        return self._national_park
+    
+    @national_park.setter
+    def national_park(self, new_national_park):
+        from classes.national_park import NationalPark
+        if isinstance(new_national_park, NationalPark):
+            self._national_park = new_national_park
+        else:
+            raise Exception 
